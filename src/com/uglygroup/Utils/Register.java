12007c5678a79796bd27efb48e85df2afc730f0a
chenmojun <953165807@qq.com>
2017-06-16 20:47:27 +0800

    \\添加注册的类

---
+++ src/com/uglygroup/Utils/Register.java
@@ -0,0 +1,18 @@
+package com.uglygroup.Utils;
+
+import java.util.Random;
+
+/**
+ * Created by Think on 2017/6/16.
+ */
+
+
+public class Register {
+
+    public static void main(String []args){
+        //System.out.println(getRandomName());
+    }
+
+
+
+}
